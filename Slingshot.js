248aa210aaa83adee0d4b9014645bc0053236f00
rahul <rahulprakashgupta2004@gmail.com>
2021-06-02 16:41:55 +0530

    vanishing pigs

--- Slingshot.js
+++ Slingshot.js
@@ -13,9 +13,15 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-
+     
     fly(){
         this.sling.bodyA = null;
+    }
+
+    attach(body){
+
+     this.sling.bodyA=body;
+
     }
 
     display(){
